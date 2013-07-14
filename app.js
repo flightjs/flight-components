e386f6c44fe2abe82fc91e73785b9796ebe75abc
Rogelio Guzman <chinog9@gmail.com>
2013-07-14 11:03:13 -0700

    Migrates into a simple express app

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
