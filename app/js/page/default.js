9f668c389105ed1016b691074da1b248aab38e2e
Rogelio Guzman <chinog9@gmail.com>
2013-07-13 18:48:58 -0700

    Adds style similar to Flightjs page

--- app/js/page/default.js
+++ app/js/page/default.js
@@ -2,18 +2,19 @@ define(function (require) {
 
   'use strict';
 
-   var flightComponents = require('bower_components/flight-components/lib/flight-components');
-   var uiFlightComponents = require('component/ui_flight_components');
-   var uiResultFilter = require('component/ui_result_filter');
+  var flightComponents = require('bower_components/flight-components/lib/flight-components');
+  var uiFlightComponents = require('component/ui_flight_components');
+  var uiResultFilter = require('component/ui_result_filter');
 
   return initialize;
 
   function initialize() {
-     uiResultFilter.attachTo("#filter", {
-       resultSelector: '#components'
-     });
-     flightComponents.attachTo('#components');
-     uiFlightComponents.attachTo('#components');
+    $('#filter').focus();
+    uiResultFilter.attachTo("#filter", {
+      resultSelector: '#components'
+    });
+    flightComponents.attachTo('#components');
+    uiFlightComponents.attachTo('#components');
   }
 
 });
